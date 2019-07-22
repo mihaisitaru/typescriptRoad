fb0c6e7b863bf133180204d7fa30e9228b4ee0e1
mihaisitaru <mihai.sitaru@yahoo.com>
2019-07-22 22:25:32 +0300

    Finished Section 2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 function Greeter(greeting) {
     this.greeting = greeting;
 }
@@ -11,3 +12,84 @@ button.onclick = function () {
     alert(greeter.greet());
 };
 document.body.appendChild(button);
+var hasHobbies = false;
+var myRealAge;
+myRealAge = 32;
+var hobbies = ["Running", "Reading"];
+hobbies = [100];
+console.log(hobbies);
+var address = [52, "FakeStreet"];
+var Color;
+(function (Color) {
+    Color[Color["Blue"] = 0] = "Blue";
+    Color[Color["Yellow"] = 15] = "Yellow";
+    Color[Color["Red"] = 2] = "Red";
+})(Color || (Color = {}));
+var myColor = Color.Blue;
+var newColor = Color.Red + 1;
+console.log(myColor); //0
+console.log(newColor);
+var house = 'loft';
+console.log(house);
+house = { type: 'appartment', rooms: 3 };
+console.log(house);
+function returnMyAge() {
+    return myRealAge;
+}
+console.log(returnMyAge());
+function sayHi() {
+    console.log('Hi there!');
+    //return myRealAge;
+}
+function multiply(x, y) {
+    return x * y;
+}
+console.log(multiply(2, 3));
+var newMultiply;
+//newMultiply = sayHi;
+//newMultiply();
+newMultiply = multiply;
+console.log(newMultiply(5, 6));
+var userData = {
+    name: 'Mihai',
+    age: 32
+};
+console.log(userData);
+var complexObj = {
+    data: [5, 7, 9],
+    output: function (all) {
+        return this.data;
+    }
+};
+console.log(complexObj);
+// union types
+var myRealRealAge = 32;
+myRealRealAge = '33';
+// myRealRealAge = true;
+var anotherVar = 'anything';
+if (typeof anotherVar == 'number') {
+    console.log('anotherVar is of type number');
+}
+else {
+    console.log('anotherVar is not of type number, but of type: ', typeof anotherVar);
+}
+function neverSayNever() {
+    throw new Error('Error!');
+}
+var canBeNull = 12;
+canBeNull = null;
+var canAlsoBeNull;
+canAlsoBeNull = null;
+var bankAccount = {
+    money: 2000,
+    deposit: function (value) {
+        this.money += value;
+    }
+};
+var myself = {
+    name: 'Mihai',
+    bankAccount: bankAccount,
+    hobbies: ['Running', 'Reading']
+};
+myself.bankAccount.deposit(3000);
+console.log(myself);
--- script.ts
+++ script.ts
@@ -15,3 +15,114 @@ function Greeter(greeting: string) {
   };
   
   document.body.appendChild(button);
+
+  let hasHobbies: boolean = false;
+
+  let myRealAge: number;
+  myRealAge = 32;
+
+  let hobbies: any[] = ["Running", "Reading"];
+  hobbies = [100];
+  console.log(hobbies);
+  
+  let address: [number, string] = [52, "FakeStreet"];
+  enum Color {
+    Blue,
+    Yellow = 15,
+    Red = 2
+  }
+
+  let myColor: Color = Color.Blue;
+  let newColor: Color = Color.Red + 1;
+  console.log(myColor); //0
+  console.log(newColor);
+
+  let house: any = 'loft';
+  console.log(house);
+  house = {type: 'appartment', rooms: 3};
+  console.log(house);
+  
+function returnMyAge(): number {
+  return myRealAge;
+}
+
+console.log(returnMyAge());
+
+function sayHi(): void {
+  console.log('Hi there!');
+  //return myRealAge;
+}
+
+function multiply(x: number, y: number): number {
+  return x * y;
+}
+
+console.log(multiply(2, 3));
+
+let newMultiply: (val1: number, val2: number) => number;
+//newMultiply = sayHi;
+//newMultiply();
+newMultiply = multiply;
+console.log(newMultiply(5, 6));
+
+let userData: {name: string, age: number} = {
+  name: 'Mihai',
+  age: 32
+};
+
+console.log(userData);
+
+// userData = {
+//   a: 'Hi',
+//   b: 45
+// };
+
+type ComplexObj = {data: number[], output: (all: boolean) => number[]};
+
+let complexObj: ComplexObj = {
+  data: [5, 7, 9],
+  output: function (all: boolean): number[] {
+    return this.data;
+  }
+};
+console.log(complexObj);
+
+// union types
+
+let myRealRealAge: number | string = 32;
+myRealRealAge = '33';
+// myRealRealAge = true;
+
+let anotherVar = 'anything';
+if (typeof anotherVar == 'number') {
+  console.log('anotherVar is of type number');
+} else {
+  console.log('anotherVar is not of type number, but of type: ', typeof anotherVar);
+}
+
+function neverSayNever(): never {
+  throw new Error('Error!');
+}
+
+let canBeNull: number | null = 12;
+canBeNull = null;
+let canAlsoBeNull;
+canAlsoBeNull = null;
+
+type BankAccount = {money: number, deposit: (a: number) => void};
+
+let bankAccount: BankAccount = {
+  money: 2000,
+  deposit(value: number): void {
+    this.money += value;
+  }
+};
+
+let myself: {name: string, bankAccount: BankAccount, hobbies: string[]} = {
+  name: 'Mihai',
+  bankAccount: bankAccount,
+  hobbies: ['Running', 'Reading']
+};
+
+myself.bankAccount.deposit(3000);
+console.log(myself);
